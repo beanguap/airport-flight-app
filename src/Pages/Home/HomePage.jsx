4bd452dc8bdb9a8b998b696d469ed5df9adb4718
Jeriel Martinez <martinez.jeriel01@gmail.com>
2024-08-18 19:21:11 -0400

    take off

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,14 +1,15 @@
-
 import NavBar from '../../components/NavBar/NavBar.jsx';
 import './HomePage.css';
 
 const HomePage = () => (
-    <div className="home-page">
-        <div className="section">Luggage</div>
-        <div className="section">Destination/Fun Facts</div>
-        <div className="section">Flight/Trip Info</div>
-        <NavBar />
+  <div className="home-page">
+    <div className="sections-container">
+      <div className="section">Luggage</div>
+      <div className="section">Destination/Fun Facts</div>
+      <div className="section">Flight/Trip Info</div>
     </div>
+    <NavBar />
+  </div>
 );
 
 export default HomePage;
