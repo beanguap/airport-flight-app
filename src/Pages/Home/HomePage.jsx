591984fedc5870b2f47cd1341aad5c21839a5dfe
Jeriel Martinez <martinez.jeriel01@gmail.com>
2024-08-19 21:32:59 -0400

    Filter for luggage

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,11 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar.jsx';
 import './HomePage.css';
-import luggageDisplay from '../../assets/LuggageDisplay.jpg'; // Path to the image
+import luggageDisplay from '../../assets/LuggageDisplay.jpg';
 
 const HomePage = () => {
   const navigate = useNavigate();
-
+  
   const handleSectionClick = (section) => {
     if (section === 'Luggage') {
       navigate('/luggage');
@@ -19,14 +19,26 @@ const HomePage = () => {
         <div className="section" onClick={() => handleSectionClick('Luggage')}>
           <div className="section-image-container">
             <img src={luggageDisplay} alt="Luggage Display" className="section-image" />
+          </div>
+          <div className="section-title-container">
             <div className="section-title">Luggage</div>
           </div>
         </div>
         <div className="section" onClick={() => handleSectionClick('Destination')}>
-          <div className="section-title">Destination/Fun Facts</div>
+          <div className="section-image-container">
+            {/* Add an image for Destination if available */}
+          </div>
+          <div className="section-title-container">
+            <div className="section-title">Destination/Fun Facts</div>
+          </div>
         </div>
         <div className="section" onClick={() => handleSectionClick('Flight')}>
-          <div className="section-title">Flight/Trip Info</div>
+          <div className="section-image-container">
+            {/* Add an image for Flight if available */}
+          </div>
+          <div className="section-title-container">
+            <div className="section-title">Flight/Trip Info</div>
+          </div>
         </div>
       </div>
       <NavBar />
