6215622c680357a9ccd6d0a1e15475554d659696
Jeriel Martinez <martinez.jeriel01@gmail.com>
2024-09-18 13:13:35 -0400

    Changed navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,23 @@
-import { Link } from 'react-router-dom';
-import './NavBar.css';
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
-    return (
-        <nav className="navbar">
-            <Link to="/flight-tracker" className="nav-icon">
-                🏠
-            </Link>
-            <Link to="/" className="nav-icon">
-                🏠
-            </Link>
-            <Link to="/luggage" className="nav-icon">
-                🎒
-            </Link>
-            <Link to="/passport" className="nav-icon">
-                📘
-            </Link>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <Link to="/flight-tracker" className="nav-icon">
+        ✈️
+      </Link>
+      <Link to="/" className="nav-icon">
+        🏠
+      </Link>
+      <Link to="/luggage" className="nav-icon">
+        🎒
+      </Link>
+      <Link to="/passport" className="nav-icon">
+        📘
+      </Link>
+    </nav>
+  );
 };
 
 export default NavBar;
